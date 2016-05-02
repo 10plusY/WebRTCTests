011357812aa7ac0c9c3c3250dbb580c5f30f26fe
10plusY <grigsmusic1990@gmail.com>
2016-05-02 12:33:36 -0400

    other video element

--- src/test2/core.cljs
+++ src/test2/core.cljs
@@ -5,43 +5,53 @@
 
 (defonce constraints
   #js {:video
-        #js {:mandatory
-             #js {:maxWidth 320
-                  :maxHeight 180}}})
+    #js {:mandatory
+       #js {:maxWidth 320
+            :maxHeight 180}}})
+
+(defonce google-stun-server
+  #js {:iceServers [{:url "stun:stun.l.google.com:19302"}]})
+
+(def peer-connection nil)
 
 (defn open-stream []
   (let [success (fn [stream]
-                  (let [video (. js/document (querySelector "video"))
+                  (let [video (. js/document (getElementById "in"))
                         src-url (.. js/window -URL (createObjectURL stream))]
                     (set! (.-src video) src-url)
                     (set! (.-onloadedmetadata video) (fn [e] (.play video)))))
         failure (fn [error]
-                  (. js/console (log "Cannot get user media...")))]
+                  (println "Cannot get user media...")
+                  (println (.message error)))]
     (.. js/navigator (webkitGetUserMedia constraints
                                          success
                                          failure))))
 
 ;network exchange
 
-;create and configure RTCPeerConnection
-;; (def peer-connection
-;;   (js/webkitRTCPeerConnection.
-;;     #js {:iceServers [{:url "stun:stun.l.google.com:19302"}]}
-;;     #js {:optional [{:RTPDataChannels true}]}))
+(defn open-peer-connection [servers]
+  (let [conn (js/webkitRTCPeerConnection.
+                servers #js {:optional [{:RTPDataChannels true}]})]
+    (set! peer-connection conn)))
+
 
 ;when ice candidate is found
 
 ;when signal channel message received
 
-
-
 (defn rtc-view []
   (fn []
     [:div.container
      [:h1 "WebRTC Test"]
-     [:video]
-     [:button {:on-click (fn [e] (open-stream))}
-       "Connect"]]))
+     [:div.in
+      [:h2 "In"]
+      [:video {:id "in"}]
+      [:button {:on-click (fn [e] (open-stream))}
+       "Connect"]]
+     [:div.out
+      [:h2 "Out"]
+      [:video {:id "out"}]
+      [:button "Receive"]]]))
 
 (r/render-component [rtc-view]
   (. js/document (getElementById "app")))
